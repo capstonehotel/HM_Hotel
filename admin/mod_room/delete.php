744981eb0df5a7be7463daa1e99e44c379388ac4
capstonehotel <ungonkathleen@gmail.com>
2024-07-05 13:41:11 +0800

    commit

--- admin/mod_room/delete.php
+++ admin/mod_room/delete.php
@@ -3,81 +3,75 @@ echo '<script src="../sweetalert2.all.min.js"></script>';
 
 if (isset($_GET['id']) && !isset($_GET['confirm'])) {
     $id = $_GET['id'];
-    $sql = "DELETE FROM tblreservation WHERE ROOMID = $id";
-    if ($connection->query($sql) === TRUE) {
-        $sql1 = "DELETE FROM tblroom WHERE ROOMID = $id";
-        if ($connection->query($sql1) === TRUE) {
-            
-            echo '<script>
-            document.addEventListener("DOMContentLoaded", function() {
-                Swal.fire({
-                    title: "Are you sure?",
-                    text: "You won\'t be able to revert this!",
-                    icon: "warning",
-                    showCancelButton: true,
-                    confirmButtonColor: "#3085d6",
-                    cancelButtonColor: "#d33",
-                    confirmButtonText: "Yes, delete it!"
-                }).then((result) => {
-                    if (result.isConfirmed) {
+    echo '<script>
+    document.addEventListener("DOMContentLoaded", function() {
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won\'t be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: "delete.php",
+                    type: "POST",
+                    data: { id: ' . $id . ' },
+                    success: function(response) {
+                        if(response === "success") {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "The room has been deleted.",
+                                icon: "success"
+                            }).then(() => {
+                                window.location.href = "index.php";
+                            });
+                        } else {
+                            Swal.fire({
+                                title: "Error!",
+                                text: "There was an error deleting the room.",
+                                icon: "error"
+                            });
+                        }
+                    },
+                    error: function(xhr, status, error) {
                         Swal.fire({
-                            title: "Deleted!",
-                            text: "The room has been deleted.",
-                            icon: "success"
-                        }).then(() => {
-                            window.location.href = "index.php?confirm=true&id=' . $id . '";
+                            title: "Error!",
+                            text: "There was an error deleting the room.",
+                            icon: "error"
                         });
-                    } else {
-                        // User clicked "Cancel", do nothing (no action needed)
-                        window.location.href = "index.php";
                     }
                 });
-            });
-            </script>';
-    
-}
-    }
-}
-
-if (isset($_GET['confirm']) && $_GET['confirm'] === 'true' && isset($_GET['id'])) {
-    $id = $_GET['id'];
-
-    $sql = "DELETE FROM tblreservation WHERE ROOMID = $id";
-
-    if ($connection->query($sql) === TRUE) {
-        $sql1 = "DELETE FROM tblroom WHERE ROOMID = $id";
-        if ($connection->query($sql1) === TRUE) {
-            echo '<script>
-            swal({
-                title: "Deleted Successfully!",
-                text: "The record has been deleted.",
-                icon: "success"
-            }).then(() => {
+            } else {
                 window.location.href = "index.php";
-            });
-            </script>';
-        } else {
-            echo '<script>
-            swal({
-                title: "Error!",
-                text: "Deleting unsuccessful.",
-                icon: "error"
-            }).then(() => {
-                window.location.href = "index.php";
-            });
-            </script>';
-        }
-    } else {
-        echo '<script>
-        swal({
-            title: "Error!",
-            text: "Deleting unsuccessful.",
-            icon: "error"
-        }).then(() => {
-            window.location.href = "index.php";
+            }
         });
-        </script>';
+    });
+    </script>';
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
+    $id = $_POST['id'];
+    
+    // Make sure to use parameterized queries to prevent SQL injection
+    $stmt = $connection->prepare("DELETE FROM tblreservation WHERE ROOMID = ?");
+    $stmt->bind_param("i", $id);
+    if ($stmt->execute()) {
+        $stmt->close();
+        $stmt1 = $connection->prepare("DELETE FROM tblroom WHERE ROOMID = ?");
+        $stmt1->bind_param("i", $id);
+        if ($stmt1->execute()) {
+            echo 'success';
+        } else {
+            echo 'error';
+        }
+        $stmt1->close();
+    } else {
+        echo 'error';
     }
+
 
     $connection->close();
 }
