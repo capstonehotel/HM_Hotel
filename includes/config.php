754aa23e0c2ff132da28b3d099eb862b4f85db45
capstonehotel <ungonkathleen@gmail.com>
2024-07-11 17:12:28 +0800

    commit

--- includes/config.php
+++ includes/config.php
@@ -17,7 +17,7 @@ $docRoot =$_SERVER['DOCUMENT_ROOT'];
 
 $webRoot  = str_replace(array($docRoot, 'includes/config.php'), '', $thisFile);
 $srvRoot  = str_replace('config/config.php','', $thisFile);
-$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME,3306);
+$connection = mysqli_connect(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
 
 
 define('WEB_ROOT', $webRoot);
