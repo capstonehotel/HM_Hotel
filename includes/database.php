6fceab0054ec3d008d3c5b8ece39cc712cb20c47
capstonehotel <ungonkathleen@gmail.com>
2024-07-13 11:14:24 +0800

    commit

--- includes/database.php
+++ includes/database.php
@@ -23,7 +23,7 @@ class Database {
 	}
 	
 	public function open_connection() {
-		$this->conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME,);
+		$this->conn = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME,3306);
 		if(!$this->conn){
 			echo "Problem in database connection! Contact administrator!";
 			exit();
