667bd873c9b9e8e697985abbada7b007557bbacc
capstonehotel <ungonkathleen@gmail.com>
2024-07-14 04:13:50 +0000

    commit

--- admin/mod_room/index.php
+++ admin/mod_room/index.php
@@ -1,7 +1,7 @@
 <?php
 require_once("../../includes/initialize.php");
  if (!isset($_SESSION['ADMIN_ID'])){
- 	redirect(WEB_ROOT ."admin/login.php");
+ 	redirect("login.php");
  }
 $view = (isset($_GET['view']) && $_GET['view'] != '') ? $_GET['view'] : '';
 $title ="Room";
