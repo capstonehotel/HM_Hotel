27925008f4d5afababd36d3d58756ad6834e852d
capstonehotel <ungonkathleen@gmail.com>
2024-07-14 04:45:35 +0000

    commit

--- admin/backendTemplate.php
+++ admin/backendTemplate.php
@@ -20,7 +20,7 @@
     <!-- Custom styles for this template-->
     <link href="../css/sb-admin-2.min.css" rel="stylesheet">
 
- <link href="../assets/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+ <link href="../assets/css/bootstrap.min.css" rel="stylesheet"  crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" href="../../assets/bootstrap/css/bootstrap.min.css">
 
 <link href="../css/dataTables.bootstrap.css" rel="stylesheet" media="screen">
