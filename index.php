74ca67a1fff48b9545e7ff367de1a9b63f531e2c
capstonehotel <ungonkathleen@gmail.com>
2024-07-18 05:54:51 +0000

    commit

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 require_once ("includes/config.php");
 require_once("includes/database.php");
 require_once("includes/accomodation.php");
+require_once("includes/functions.php");
 
 // require_once("includes/initialize.php");
 $content='home.php';
