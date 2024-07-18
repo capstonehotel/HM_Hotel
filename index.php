093b196d346dafd91dddd97cd5079677f31665e0
capstonehotel <ungonkathleen@gmail.com>
2024-07-18 04:25:42 +0000

    commit

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 require_once ("includes/config.php");
 require_once("includes/database.php");
-require_once("includes/initialize.php");
+// require_once("includes/initialize.php");
 $content='home.php';
 $view = (isset($_GET['p']) && $_GET['p'] != '') ? $_GET['p'] : '';
 $account = 'guest/update.php';
