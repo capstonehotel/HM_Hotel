a4ea7ea83e7821b8112de2fadb02a147a43e64fa
capstonehotel <ungonkathleen@gmail.com>
2024-07-18 06:15:15 +0000

    commit

--- booking/index.php
+++ booking/index.php
@@ -1,6 +1,5 @@
 <?php
-require_once("../includes/config.php"); 
-require_once("../includes/reserve.php"); 
+require_once("../includes/initialize.php"); 
 // $menus=array("Home Page"=>"home.php","About Us"=>"about.php","Booking"=>"booking.php","Admin"=>"services.php","Latest News"=>"latest.php","contacts"=>"contact.php");
 $view = (isset($_GET['view']) && $_GET['view'] != '') ? $_GET['view'] : '';
 $account = '../guest/update.php'; 
