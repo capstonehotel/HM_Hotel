cd3b736bb57999c980197be407c5f4cef8b36927
capstonehotel <ungonkathleen@gmail.com>
2024-07-25 14:06:35 +0000

    commit

--- index.php
+++ index.php
@@ -3,9 +3,6 @@ require_once ("includes/config.php");
 require_once("includes/database.php");
 require_once("includes/accomodation.php");
 require_once("includes/functions.php");
-
-//later here where we are going to put our class session
-require_once("session.php");
 require_once("user.php");
 require_once("pagination.php");
 require_once("paginsubject.php");
