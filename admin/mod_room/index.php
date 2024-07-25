318025822130f4dd5dac1bdce5751ad6968c8edc
capstonehotel <ungonkathleen@gmail.com>
2024-07-25 14:58:55 +0000

    commit

--- admin/mod_room/index.php
+++ admin/mod_room/index.php
@@ -1,5 +1,20 @@
 <?php
-require_once("../../includes/initialize.php");
+// require_once("../../includes/initialize.php");
+// load config file first 
+require_once("../../includes/config.php");
+//load basic functions next so that everything after can use them
+require_once("../../includes/functions.php");
+//later here where we are going to put our class session
+require_once("../../includes/session.php");
+require_once("../../includes/user.php");
+require_once("../../includes/pagination.php");
+require_once("../../includes/paginsubject.php");
+require_once("../../includes/accomodation.php");
+require_once("../../includes/guest.php");
+require_once("../../includes/reserve.php"); 
+require_once("../../includes/setting.php");
+//Load Core objects
+require_once("../../includes/database.php");
  if (!isset($_SESSION['ADMIN_ID'])){
  	redirect("../login.php");
  }
