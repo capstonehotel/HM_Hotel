afd072a571dd2df0390e3b5d80b08c76671f1156
capstonehotel <ungonkathleen@gmail.com>
2024-07-25 18:01:44 +0000

    commit

--- admin/mod_room/delete.php
+++ admin/mod_room/delete.php
@@ -1,19 +1,7 @@
 <?php
 // require_once("../../includes/initialize.php");
 // load config file first 
-require_once("../../includes/config.php");
-//load basic functions next so that everything after can use them
-require_once("../../includes/functions.php");
-//later here where we are going to put our class session
-require_once("../../includes/session.php");
-require_once("../../includes/user.php");
-require_once("../../includes/pagination.php");
-require_once("../../includes/paginsubject.php");
-require_once("../../includes/accomodation.php");
-require_once("../../includes/guest.php");
-require_once("../../includes/reserve.php"); 
-require_once("../../includes/setting.php");
-//Load Core objects
+
 require_once("../../includes/database.php");
 echo '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>';
 if (isset($_GET['id']) && isset($_GET['confirm']) && $_GET['confirm'] === 'true') {
