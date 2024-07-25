cf0a040ee2d76179dcd937e71002069f0f52c9c1
capstonehotel <ungonkathleen@gmail.com>
2024-07-25 14:40:37 +0000

    commit

--- booking/booking.php
+++ booking/booking.php
@@ -136,7 +136,7 @@ if(isset($_GET['id'])){
              if (isset($_SESSION['monbela_cart'])){
               ?>
                 <a  href="../index.php?p=rooms" class="btn btn-primary" align="right"name="clear">Add Another Room</a>
-                <a  href="../index.php?p=bookAroom" type="submit" class="btn btn-primary" align="right"name="clear">Clear Cart</button>
+                <a  href="../index.php?p=bookAroom" type="submit" class="btn btn-primary" align="right"name="clear">Clear Cart</a>
              <?php
              
               if (isset($_SESSION['GUESTID'])){
