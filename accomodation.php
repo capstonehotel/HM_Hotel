2d6ef21096435e39b45fda49342581f4ec4bc1f2
capstonehotel <ungonkathleen@gmail.com>
2024-07-25 08:09:18 +0000

    commit

--- accomodation.php
+++ accomodation.php
@@ -1,7 +1,19 @@
 <?php
-require_once("includes/accomodation.php");
+// load config file first 
+require_once("includes/config.php");
+//load basic functions next so that everything after can use them
 require_once("includes/functions.php");
-
+//later here where we are going to put our class session
+require_once("includes/session.php");
+require_once("includes/user.php");
+require_once("includes/pagination.php");
+require_once("includes/paginsubject.php");
+require_once("includes/accomodation.php");
+require_once("includes/guest.php");
+require_once("includes/reserve.php"); 
+require_once("includes/setting.php");
+//Load Core objects
+require_once("includes/database.php");
 $msg = "";
 
 if(isset($_POST['booknow'])){
