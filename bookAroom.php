32c71d16eaeb2eb3c5580c77d0db42fdc4f4a322
capstonehotel <ungonkathleen@gmail.com>
2024-07-26 09:05:16 +0000

    commit

--- bookAroom.php
+++ bookAroom.php
@@ -292,10 +292,6 @@ align-items: center;
 </div>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-
-
-
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
 <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script> -->
