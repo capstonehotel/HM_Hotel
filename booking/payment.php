b547b3e04fdf8798d4719ce5a90d06a4488bc7c5
capstonehotel <ungonkathleen@gmail.com>
2024-07-27 04:16:04 +0000

    commit

--- booking/payment.php
+++ booking/payment.php
@@ -1,6 +1,7 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <?php
 
 if (!isset($_SESSION['monbela_cart'])) {
@@ -162,7 +163,13 @@ $_SESSION['GUESTID'] =   $lastguest;
 
             ?> 
 
-<script type="text/javascript"> alert("Booking is successfully submitted!");</script>
+<script>
+ Swal.fire({
+  title: 'Success!',
+  text: 'Booking is successfully submitted!',
+  icon: 'success',
+  confirmButtonText: 'OK'
+});</script>
 
             <?php
             
