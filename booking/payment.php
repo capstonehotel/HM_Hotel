563d5419cc0dd08d304447c532d1c699d9dd1bb5
capstonehotel <ungonkathleen@gmail.com>
2024-07-27 04:08:16 +0000

    commit

--- booking/payment.php
+++ booking/payment.php
@@ -85,123 +85,121 @@ if(isset($_POST['btnsubmitbooking'])){
 <script type="text/javascript"> alert("Booking is successfully submitted!");</script>
 
     <?php
-    redirect(WEB_ROOT."index.php");
+    redirect(WEB_ROOT."../index.php");
 }
 ?>
 
 <div class="card rounded" style="padding: 10px;">
-  <div class="pagetitle">   
-    <h1>Billing Details</h1> 
-  </div>
-  <nav aria-label="breadcrumb">
-    <ol class="breadcrumb" style="margin-top: 10px;">
-      <li class="breadcrumb-item"><a href="<?php echo WEB_ROOT ;?>index.php">Home</a></li>
-      <li class="breadcrumb-item"><a href="<?php echo WEB_ROOT ;?>booking/">Booking Cart</a></li> 
-      <li class="breadcrumb-item active" aria-current="page">Billing Details</li>
-    </ol>
-  </nav>
-  <div class="container">
-    <div class="row">
-      <form id="bookingForm" action="../index.php" method="post" name="personal">
-        <div class="col-md-8 col-sm-4">
-          <div class="col-md-12">
-            <label>Name:</label>
-            <?php echo $_SESSION['name'] . ' ' . $_SESSION['last']; ?>
-          </div>
-          <div class="col-md-12">
-            <label>Address:</label>
-            <?php echo isset($_SESSION['city']) ? $_SESSION['city'] : ' ' . ' ' . (isset($_SESSION['address']) ? $_SESSION['address'] : ' '); ?>
-          </div>
-          <div class="col-md-12">
-            <label>Phone #:</label>
-            <?php echo $_SESSION['phone']; ?>
-          </div>
-        </div>
-        <div class="col-md-4 col-sm-2">
-          <div class="col-md-12">
-            <label>Transaction Date:</label>
-            <?php echo date("m/d/Y"); ?>
-          </div>
-          <div class="col-md-12">
-            <label>Transaction Id:</label>
-            <?php echo $_SESSION['confirmation']; ?>
-          </div>
-        </div>
-        <div class="table-responsive">
-          <table class="table table-striped">
-            <thead>
-              <tr>
-                <th>Room</th>
-                <th>Checked in</th>
-                <th>Checked out</th>
-                <th>Price</th>
-                <th>Night(s)</th>
-                <th>Subtotal</th>
-              </tr>
-            </thead>
-            <tbody>
-              <?php
-              if (isset($_SESSION['monbela_cart'])) {
-                $count_cart = count($_SESSION['monbela_cart']);
-                for ($i = 0; $i < $count_cart; $i++) {
-                  $query = "SELECT * FROM `tblroom` r, `tblaccomodation` a WHERE r.`ACCOMID`=a.`ACCOMID` AND ROOMID=" . $_SESSION['monbela_cart'][$i]['monbelaroomid'];
-                  $mydb->setQuery($query);
-                  $cur = $mydb->loadResultList();
-                  foreach ($cur as $result) {
-              ?>
-                <tr>
-                  <td><?php echo $result->ROOM . ' ' . $result->ROOMDESC; ?></td>
-                  <td><?php echo $_SESSION['monbela_cart'][$i]['monbelacheckin']; ?></td>
-                  <td><?php echo $_SESSION['monbela_cart'][$i]['monbelacheckout']; ?></td>
-                  <td>₱<?php echo $result->PRICE; ?></td>
-                  <td><?php echo $_SESSION['monbela_cart'][$i]['monbeladay']; ?></td>
-                  <td>₱ <output><?php echo $_SESSION['monbela_cart'][$i]['monbelaroomprice']; ?></output></td>
-                </tr>
-              <?php
-                  }
-                }
-              }
-              ?>
-             </tbody>
-          </table>
-        </div>
-        <div class="row"> 
-          <div class="col-md-12 text-right">
-            <h3 align="right">Total: &#8369; <?php echo $_SESSION['pay']; ?></h3>
-          </div>
-        </div>
-        <div class="pull-right flex-end" align="right">
-          <button type="button" class="btn btn-primary" id="submitBtn">Submit Booking</button>
-        </div>
-      </form>
+    <div class="pagetitle">   
+        <h1>Billing Details</h1> 
     </div>
-  </div>
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb" style="margin-top: 10px;">
+        <li class="breadcrumb-item"><a href="<?php echo WEB_ROOT; ?>index.php">Home</a></li>
+        <li class="breadcrumb-item"><a href="<?php echo WEB_ROOT; ?>booking/">Booking Cart</a></li> 
+        <li class="breadcrumb-item active" aria-current="page">Billing Details</li>
+      </ol>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <form action="index.php?view=payment" method="post" name="personal">
+                <div class="col-md-8 col-sm-4">
+                    <div class="col-md-12">
+                        <label>Name:</label>
+                        <?php echo $_SESSION['name'] . ' ' . $_SESSION['last']; ?>
+                    </div>
+                    <div class="col-md-12">
+                        <label>Address:</label>
+                        <?php echo isset($_SESSION['city']) ? $_SESSION['city'] . ' ' : ''; ?>
+                        <?php echo isset($_SESSION['address']) ? $_SESSION['address'] : ''; ?>
+                    </div>
+                    <div class="col-md-12"> 
+                        <label>Phone #:</label>
+                        <?php echo $_SESSION['phone']; ?>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-2">
+                    <div class="col-md-12">
+                        <label>Transaction Date:</label>
+                        <?php echo date("m/d/Y"); ?>
+                    </div>
+                    <div class="col-md-12">
+                        <label>Transaction Id:</label>
+                        <?php echo $_SESSION['confirmation']; ?>
+                    </div>
+                </div>
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <td>Room</td>
+                                <td>Checked in</td>
+                                <td>Checked out</td>
+                                <td>Price</td>
+                                <td>Night(s)</td>
+                                <td>Subtotal</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $payable = 0;
+                            if (isset($_SESSION['monbela_cart'])) { 
+                                $count_cart = count($_SESSION['monbela_cart']);
+                                for ($i = 0; $i < $count_cart; $i++) {  
+                                    $query = "SELECT * FROM `tblroom` r, `tblaccomodation` a WHERE r.`ACCOMID`=a.`ACCOMID` AND ROOMID=" . $_SESSION['monbela_cart'][$i]['monbelaroomid'];
+                                    $mydb->setQuery($query);
+                                    $cur = $mydb->loadResultList(); 
+                                    foreach ($cur as $result) {
+                            ?>
+                                        <tr>
+                                            <td><?php echo $result->ROOM . ' ' . $result->ROOMDESC; ?></td>
+                                            <td><?php echo date_format(date_create($_SESSION['monbela_cart'][$i]['monbelacheckin']), "m/d/Y"); ?></td>
+                                            <td><?php echo date_format(date_create($_SESSION['monbela_cart'][$i]['monbelacheckout']), "m/d/Y"); ?></td>
+                                            <td><?php echo ' &#8369 ' . $result->PRICE; ?></td>
+                                            <td><?php echo $_SESSION['monbela_cart'][$i]['monbeladay']; ?></td>
+                                            <td><?php echo ' &#8369 ' . $_SESSION['monbela_cart'][$i]['monbelaroomprice']; ?></td>
+                                        </tr>
+                            <?php
+                                        $payable += $_SESSION['monbela_cart'][$i]['monbelaroomprice'];
+                                    }
+                                } 
+                                $_SESSION['pay'] = $payable;
+                            }
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="row"> 
+                    <h3 align="right">Total: &#8369 <?php echo $_SESSION['pay']; ?></h3>
+                </div>
+                <div class="pull-right flex-end" align="right">
+                    <button type="button" class="btn btn-primary" align="right" data-toggle="modal" data-target="#confirmModal">Submit Booking</button>
+                </div>
+            </form>
+        </div>  
+    </div>
 </div>
 
-<script>
-document.getElementById('submitBtn').addEventListener('click', function() {
-  Swal.fire({
-    title: 'Are you sure?',
-    text: "Do you want to submit the booking?",
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Yes, submit it!',
-    cancelButtonText: 'No, cancel'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      // Add hidden input to trigger the PHP handler
-      var form = document.getElementById('bookingForm');
-      var input = document.createElement('input');
-      input.type = 'hidden';
-      input.name = 'btnsubmitbooking';
-      input.value = 'Yes';
-      form.appendChild(input);
+<script type="text/javascript">
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button[data-toggle="modal"]').addEventListener('click', function(event) {
+        event.preventDefault(); // Prevent the default action
 
-      // Submit the form
-      form.submit();
-    }
-  });
+        Swal.fire({
+            title: 'Are you sure?',
+            text: 'Do you want to submit the booking?',
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes',
+            cancelButtonText: 'No'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                // Trigger form submission
+                document.querySelector('form[name="personal"]').submit();
+            }
+        });
+    });
 });
 </script>
