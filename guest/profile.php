9478b2b3dfe39fcd06f24642a2cc2fc92c6a6bb0
capstonehotel <ungonkathleen@gmail.com>
2024-07-29 06:15:38 +0000

    commit

--- guest/profile.php
+++ guest/profile.php
@@ -198,7 +198,7 @@ $res = $guest->single_guest($_SESSION['GUESTID']);
     </div>
   </div>
 </div>
-<!-- </body> --> -->
+<!-- </body> --> 
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.min.js" integrity="sha384-Rx+T1VzGupg4BHQYs2gCW9It+akI2MM/mndMCy36UVfodzcJcF0GGLxZIzObiEfa" crossorigin="anonymous"></script>
 
@@ -309,4 +309,15 @@ $('.dbirth').datetimepicker({
 
         }
 </script>
+<script>
+    $(document).ready(function() {
+      $('#profileModal').on('show.bs.modal', function (e) {
+        var modal = $(this);
+        modal.find('.modal-body').load('guest/profile.php', function() {
+          // Adjust form field sizes if necessary
+          $('.form-control').css('width', '100%');
+        });
+      });
+    });
+  </script>
 <!-- </html> -->
