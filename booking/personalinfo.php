d01833e7bff0e05f4e21283f4a980edcf55ee0ec
capstonehotel <ungonkathleen@gmail.com>
2024-07-30 03:20:27 +0000

    commit

--- booking/personalinfo.php
+++ booking/personalinfo.php
@@ -276,7 +276,27 @@ function previewImage(event) {
     </div>
 </div>
 
+<div style="position: relative; width: 300px; margin: 20px;">
+        <input name="pass" type="password" class="form-control" id="password" placeholder="Enter password" style="padding-right: 40px;">
+        <span class="fa fa-eye toggle-password" onclick="togglePasswordVisibility()"></span>
+    </div>
 
+    <script>
+        function togglePasswordVisibility() {
+            var passwordInput = document.getElementById("password");
+            var toggleIcon = document.querySelector(".toggle-password");
+
+            if (passwordInput.type === "password") {
+                passwordInput.type = "text";
+                toggleIcon.classList.remove("fa-eye");
+                toggleIcon.classList.add("fa-eye-slash");
+            } else {
+                passwordInput.type = "password";
+                toggleIcon.classList.remove("fa-eye-slash");
+                toggleIcon.classList.add("fa-eye");
+            }
+        }
+    </script>
 
 
 			          <div class="form-group">
