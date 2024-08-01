bb83809675e420ea8800ba5fb99cf8f37d044d4b
capstonehotel <ungonkathleen@gmail.com>
2024-08-01 23:47:32 +0000

    commit

--- admin/mod_accomodation/list.php
+++ admin/mod_accomodation/list.php
@@ -19,13 +19,12 @@
         background-color: #f8f9fa;
     }
 </style>
-
 <div class="container-fluid">
     <div class="card shadow mb-4">
         <div class="card-header py-3" style="display: flex; align-items: center;">
-            <h6 class="m-0 font-weight-bold text-primary">Accommodation List</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Accomodation List</h6>
             <div style="display: flex; width: 100%; justify-content: flex-end;">
-                <a href="index.php?view=add" class="btn btn-sm btn-primary">Add New Accommodation</a>
+                <a href="index.php?view=add" class="btn btn-sm btn-primary">Add New Accomodation</a>
             </div>
         </div>
         <div class="card-body">
@@ -40,18 +39,18 @@
                     </thead>
                     <tbody>
                     <?php
-                        $query = "SELECT * FROM tblaccommodation";
+                        $query = "SELECT * FROM tblaccomodation";
                         $result = mysqli_query($connection, $query);
                         if ($result) {
                             while ($row = mysqli_fetch_assoc($result)) {
                     ?>
                         <tr>
-                            <td><?php echo $row['ACCOMMODATION']; ?></td>
-                            <td><?php echo $row['ACCOMDESC']; ?></td>
+                            <td><?php echo $row['ACCOMODATION'];?></td>
+                            <td><?php echo $row['ACCOMDESC'];?></td>
                             <td style="display: flex;">
-                                <a class="btn-sm btn btn-primary mr-2" href="index.php?view=edit&id=<?php echo $row['ACCOMID']; ?>">View/Edit</a>
-                                <a class="btn-sm btn btn-danger mr-2" href="index.php?view=delete&id=<?php echo $row['ACCOMID']; ?>">Delete</a>
-                            </td>
+			            		<a class="btn-sm btn btn-primary mr-2" href="index.php?view=edit&id=<?php echo $row['ACCOMID']; ?>">View/Edit</a>
+			            		<a class="btn-sm btn btn-danger mr-2" href="index.php?view=delete&id=<?php echo $row['ACCOMID']; ?>">Delete</a>
+			            	</td>
                         </tr>
                     <?php } }?>
                     </tbody>
