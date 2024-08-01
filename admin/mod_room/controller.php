9983a0d8fee2e2c5794681ddbc846495d587d1e0
capstonehotel <ungonkathleen@gmail.com>
2024-08-01 22:56:58 +0000

    commit

--- admin/mod_room/controller.php
+++ admin/mod_room/controller.php
@@ -180,9 +180,9 @@ function editImg (){
 					$rm->ROOMIMAGE = $location;
 					$rm->update($rm_id); 
 					
-					$response = array('success' => true, 'message' => 'Room Image Updated successfully!');
-					echo json_encode($response);
-					exit;
+				 	message("Room Image Upadated successfully!", "success");
+				 	unset($_SESSION['id']);
+				 	 redirect("index.php");
  			}
  		}
  }			 
