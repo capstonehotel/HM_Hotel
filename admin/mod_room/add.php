bc602ca516a07de58ddc895f8c3196cdc6a2a98a
capstonehotel <ungonkathleen@gmail.com>
2024-08-02 06:02:15 +0000

    commit

--- admin/mod_room/add.php
+++ admin/mod_room/add.php
@@ -172,9 +172,9 @@ if (isset($_POST['save_room'])) {
             </div>
             <div class="form-group">
               <div class="col-md-12 col-sm-12">
-                <label class="col-md-4 control-label" for="image">Upload Image:</label><br>
+                <label class="col-md-4 control-label" for="image">Upload Image:</label>
                 <div class="col-md-12">
-                  <input required type="file" name="image" id="image" accept="image/*">
+                  <input required type="file" name="image" id="image" accept="image/*" align:"left;">
                   <img src="#" alt="Image Preview" id="image-preview" style="display: none; max-width: 100%; max-height: 200px;">
                   <script>
                     const fileInput = document.getElementById('image');
