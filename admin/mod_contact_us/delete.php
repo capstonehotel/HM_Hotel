a9b9c461f1918b374652789cce5a734a7d4d946c
capstonehotel <ungonkathleen@gmail.com>
2024-08-02 00:19:14 +0000

    commit

--- admin/mod_contact_us/delete.php
+++ admin/mod_contact_us/delete.php
@@ -1,4 +1,18 @@
 <?php
+require_once("../../includes/config.php");
+//load basic functions next so that everything after can use them
+require_once("../../includes/functions.php");
+//later here where we are going to put our class session
+require_once("../../includes/session.php");
+require_once("../../includes/user.php");
+require_once("../../includes/pagination.php");
+require_once("../../includes/paginsubject.php");
+require_once("../../includes/accomodation.php");
+require_once("../../includes/guest.php");
+require_once("../../includes/reserve.php"); 
+require_once("../../includes/setting.php");
+//Load Core objects
+require_once("../../includes/database.php");
 
 echo '<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>';
 // Check if 'id' is set in the query string
