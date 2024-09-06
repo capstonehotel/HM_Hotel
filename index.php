3c8a2efd967cb4cc394adc0900c316732fd159bf
capstonehotel <ungonkathleen@gmail.com>
2024-09-06 13:06:45 -0500

    commit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 // error_reporting(E_ALL);
 // require_once("includes/initialize.php");
 // include 'config.php'; 
-require_once("includes/config.php");
+// require_once("includes/config.php");
 // //load basic functions next so that everything after can use them
 // require_once("includes/functions.php");
 // //later here where we are going to put our class session
