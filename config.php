6f59407a5fe8a58f9bf0912f40d399a211cec3c5
capstonehotel <ungonkathleen@gmail.com>
2024-09-06 00:53:50 -0500

    commit

--- config.php
+++ config.php
@@ -23,7 +23,7 @@ $docRoot = $_SERVER['DOCUMENT_ROOT'];
 $webRoot = str_replace(array($docRoot, 'includes/config.php'), '', $thisFile);
 $srvRoot = str_replace('config/config.php', '', $thisFile);
 
-define('WEB_ROOT', $webRoot);
-define('SRV_ROOT', $srvRoot);
+// define('WEB_ROOT', $webRoot);
+// define('SRV_ROOT', $srvRoot);
 ?>
 
