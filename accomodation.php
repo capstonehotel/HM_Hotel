2670fbe23d55003e343d66f088dd26e9c68e5cba
capstonehotel <ungonkathleen@gmail.com>
2024-09-06 13:43:39 -0500

    commit

--- accomodation.php
+++ accomodation.php
@@ -1,7 +1,7 @@
 <?php
 require_once ("includes/initialize.php");
 // require_once ("includes/config.php");
-// require_once("includes/database.php");
+require_once("includes/database.php");
 // require_once("includes/accomodation.php");
 // require_once("includes/functions.php");
 
