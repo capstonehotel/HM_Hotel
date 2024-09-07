f84464da98c389246192c50d060707ce6c43883f
capstonehotel <ungonkathleen@gmail.com>
2024-09-07 07:12:24 -0500

    commit

--- booking/index.php
+++ booking/index.php
@@ -22,7 +22,7 @@ require_once("../includes/initialize.php");
 // $menus=array("Home Page"=>"home.php","About Us"=>"about.php","Booking"=>"booking.php","Admin"=>"services.php","Latest News"=>"latest.php","contacts"=>"contact.php");
 $view = (isset($_GET['view']) && $_GET['view'] != '') ? $_GET['view'] : '';
 $account = '../guest/update.php'; 
-$small_nav = '../small-navbar.php';
+$small_nav = '../theme/small-navbar.php';
 switch ($view) {
 	case 'booking' :
 	    $title="Booking";
