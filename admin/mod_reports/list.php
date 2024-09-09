831ceb9b1543ddc17a3bb42e415a71a1f64b74e2
capstonehotel <ungonkathleen@gmail.com>
2024-09-08 23:21:47 -0500

    commit

--- admin/mod_reports/list.php
+++ admin/mod_reports/list.php
@@ -64,8 +64,9 @@
                                             <td align="center"><?php echo $row['SPRICE']; ?></td>
                                             <td align="center"><?php echo $row['STATUS']; ?></td>
                                             <td align="center">
-                                                <a href="printreport.php?code=<?php echo $confirmation_code; ?>" target="_blank"class="btn btn-sm btn-primary"><i class="icon-edit"></i> View</a>
-                                                <a href="printreport.php?code=<?php echo $confirmation_code; ?>" target="_blank" class="btn btn-primary">View Report</a>
+                                            <a href="printreport.php?code=<?php echo $row['CONFIRMATIONCODE']; ?>" target="_blank" class="btn btn-sm btn-primary">
+    <i class="icon-print"></i> Print
+</a>
 
                                                 <?php if($_SESSION['ADMIN_UROLE']=="Administrator"){ ?>
                                                 <button type="button" class="btn btn-danger btn-sm delete-btn" data-id="<?php echo $row['CONFIRMATIONCODE']; ?>"><i class="icon-edit"></i> Delete</button>
