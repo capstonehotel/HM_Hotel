23d80214aac4d6dbfd558796a45600797a03b891
capstonehotel <ungonkathleen@gmail.com>
2024-09-09 00:00:37 -0500

    commit

--- admin/mod_reports/printreport.php
+++ admin/mod_reports/printreport.php
@@ -37,15 +37,15 @@ $result1 = mysqli_query($connection, $query1);
     <meta name="description" content="">
     <meta name="author" content="">
     <title>HM Hotel Reservation</title>
-    <link rel="stylesheet" type="text/css" href="<?php echo WEB_ROOT; ?>style.css">  
-    <link rel="stylesheet" type="text/css" href="<?php echo WEB_ROOT; ?>css/responsive.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo WEB_ROOT; ?>css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo WEB_ROOT; ?>fonts/css/font-awesome.min.css"> 
-    <link rel="stylesheet" type="text/css" href="<?php echo WEB_ROOT; ?>css/custom-navbar.min.css"> 
-    <link href="<?php echo WEB_ROOT; ?>css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
-    <link href="<?php echo WEB_ROOT; ?>css/datepicker.css" rel="stylesheet" media="screen">
-    <link href="<?php echo WEB_ROOT; ?>css/galery.css" rel="stylesheet" media="screen">
-    <link href="<?php echo WEB_ROOT; ?>css/ekko-lightbox.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="https://mcchmhtoelreservation.com/style.css">  
+    <link rel="stylesheet" type="text/css" href="https://mcchmhtoelreservation.com/css/responsive.css">
+    <link rel="stylesheet" type="text/css" href="https://mcchmhtoelreservation.com/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="https://mcchmhtoelreservation.com/fonts/css/font-awesome.min.css"> 
+    <link rel="stylesheet" type="text/css" href="https://mcchmhtoelreservation.com/css/custom-navbar.min.css"> 
+    <link href="https://mcchmhtoelreservation.com/css/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+    <link href="https://mcchmhtoelreservation.com/css/datepicker.css" rel="stylesheet" media="screen">
+    <link href="https://mcchmhtoelreservation.com/css/galery.css" rel="stylesheet" media="screen">
+    <link href="https://mcchmhtoelreservation.com/css/ekko-lightbox.css" rel="stylesheet">
     <style>
        @media print {
     body {
