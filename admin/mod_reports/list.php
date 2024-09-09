1298f77ee888bda686b3ddd1e5b111e2b69ca547
capstonehotel <ungonkathleen@gmail.com>
2024-09-08 22:40:45 -0500

    commit

--- admin/mod_reports/list.php
+++ admin/mod_reports/list.php
@@ -1,111 +1,136 @@
+<!-- Include SweetAlert2 -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<!-- Additional styling and scripts -->
+<style>
+    .table td, .table th {
+        white-space: nowrap;
+        vertical-align: middle;
+    }
+    .table thead th {
+        text-align: center;
+    }
+    .btn-sm {
+        padding: 0.25rem 0.5rem;
+    }
+    .table-responsive {
+        display: none; /* Hide table initially */
+    }
+</style>
+
 <div class="container-fluid">
-    <form id="deleteForm">
-        <div class="card shadow mb-4">
-            <div class="card-header py-3" style="display: flex; align-items: center;">
-                <h6 class="m-0 font-weight-bold text-primary">List of Checked-out Reservations</h6>
-            </div>
-            <div class="card-body">
-                <div class="table-responsive">
-                    <table class="table table-striped" id="dataTable" width="100%" cellspacing="0">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Guest</th>
-                                <th>Transaction Date</th>
-                                <th>Confirmation Code</th>
-                                <th>Total Rooms</th>
-                                <th>Status</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php 
-                                $query = "SELECT `G_FNAME`, `G_LNAME`, `TRANSDATE`, `CONFIRMATIONCODE`, `PQTY`, `STATUS`
-                                          FROM `tblpayment` p, `tblguest` g
-                                          WHERE p.`GUESTID` = g.`GUESTID` AND p.`STATUS` = 'Checkedout'
+    <div class="card shadow mb-4">
+        <div class="card-header py-3" style="display: flex; align-items: center;">
+            <h6 class="m-0 font-weight-bold text-primary">Checked-Out Reservations</h6>
+        </div>
+
+        <div class="tab-content">
+            <div class="tab-pane fade show active">
+                <div class="card-body">
+                    <div class="table-responsive" style="width: 100%;">
+                        <table class="table table-striped" id="dataTableCheckout" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Guest</th>
+                                    <th>Transaction Date</th>
+                                    <th>Confirmation Code</th>
+                                    <th>Total Rooms</th>
+                                    <th>Total Price</th>
+                                    <th>Status</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                $query = "SELECT `G_FNAME`, `G_LNAME`, `TRANSDATE`, `CONFIRMATIONCODE`, `PQTY`, `SPRICE`, `STATUS` 
+                                          FROM `tblpayment` p, `tblguest` g 
+                                          WHERE p.`GUESTID` = g.`GUESTID` AND p.`STATUS` = 'checkedout' 
                                           ORDER BY p.`TRANSDATE` DESC";
                                 $result = mysqli_query($connection, $query);
-                                $number = 0;
-                                while ($row = mysqli_fetch_assoc($result)) { $number++; ?>
-                                    <tr>
-                                        <td align="center"><?php echo $number; ?></td>
-                                        <td align="center"><?php echo $row['G_FNAME']; ?> <?php echo $row['G_LNAME']; ?></td>
-                                        <td align="center"><?php echo $row['TRANSDATE']; ?></td>
-                                        <td align="center"><?php echo $row['CONFIRMATIONCODE']; ?></td>
-                                        <td align="center"><?php echo $row['PQTY']; ?></td>
-                                        <td align="center"><?php echo $row['STATUS']; ?></td>
-                                        <td align="center">
-                                            <div class="btn-group" role="group">
-                                                <form action="printreport.php" method="GET" target="_blank">
-                                                    <input type="hidden" name="code" value="<?php echo $row['CONFIRMATIONCODE']; ?>">
-                                                    <button class="btn btn-sm btn-primary"><i class="fa fa-print"></i> Print</button>
-                                                </form>
-                                                <?php if ($_SESSION['ADMIN_UROLE'] == "Administrator") { ?>
-                                                    <button type="button" class="btn btn-sm btn-danger deleteBtn" data-id="<?php echo $row['CONFIRMATIONCODE']; ?>"><i class="icon-edit"></i> Delete</button>
+                                if (!$result) {
+                                    echo "<tr><td colspan='8'>Query failed: " . mysqli_error($connection) . "</td></tr>";
+                                } else {
+                                    $number = 0;
+                                    while ($row = mysqli_fetch_assoc($result)) {
+                                        $number++;
+                                        ?>
+                                        <tr>
+                                            <td align="center"><?php echo $number; ?></td>
+                                            <td align="center"><?php echo $row['G_FNAME']; ?> <?php echo $row['G_LNAME']; ?></td>
+                                            <td align="center"><?php echo $row['TRANSDATE']; ?></td>
+                                            <td align="center"><?php echo $row['CONFIRMATIONCODE']; ?></td>
+                                            <td align="center"><?php echo $row['PQTY']; ?></td>
+                                            <td align="center"><?php echo $row['SPRICE']; ?></td>
+                                            <td align="center"><?php echo $row['STATUS']; ?></td>
+                                            <td align="center">
+                                                <a href="index.php?view=view&code=<?php echo $row['CONFIRMATIONCODE']; ?>" class="btn btn-sm btn-primary"><i class="icon-edit"></i> View</a>
+                                                <?php if($_SESSION['ADMIN_UROLE']=="Administrator"){ ?>
+                                                <button type="button" class="btn btn-danger btn-sm delete-btn" data-id="<?php echo $row['CONFIRMATIONCODE']; ?>"><i class="icon-edit"></i> Delete</button>
                                                 <?php } ?>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                <?php } ?>  
-                        </tbody>
-                    </table>
+                                            </td>
+                                        </tr>
+                                    <?php } 
+                                } ?>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-    </form>
+    </div>
 </div>
 
-<!-- SweetAlert and AJAX script for deletion -->
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<!-- Initialize DataTables -->
 <script>
-    $(document).ready(function() {
-       
+$(document).ready(function() {
+    // Initialize DataTables for check-out tab
+    $('#dataTableCheckout').DataTable({
+        "paging": true,
+        "searching": true,
+        "lengthChange": true,
+        "pageLength": 10
+    });
 
-        // Handle delete button click
-        $('.deleteBtn').on('click', function() {
-            var confirmationCode = $(this).data('id');
-            Swal.fire({
-                title: 'Are you sure?',
-                text: "You won't be able to revert this!",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Yes, delete it!'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    $.ajax({
-                        url: 'delete.php',
-                        type: 'GET',
-                        data: { confirm: 'true', id: confirmationCode },
-                        success: function(response) {
-                            var data = JSON.parse(response);
-                            if (data.status == 'success') {
-                                Swal.fire(
-                                    'Deleted!',
-                                    'The reservation has been deleted.',
-                                    'success'
-                                );
-                                // Reload the table or remove the row
-                                location.reload();
-                            } else {
-                                Swal.fire(
-                                    'Error!',
-                                    data.message,
-                                    'error'
-                                );
-                            }
-                        },
-                        error: function() {
-                            Swal.fire(
-                                'Error!',
-                                'Failed to delete the reservation.',
-                                'error'
-                            );
-                        }
-                    });
-                }
-            });
+    // Show table after initialization
+    $('.table-responsive').show();
+
+    // Event listener for deleting a reservation
+    $(document).on('click', '.delete-btn', function() {
+        var confirmationCode = $(this).data('id');
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $.ajax({
+                    url: 'delete.php',
+                    type: 'GET',
+                    data: { id: confirmationCode, confirm: 'true' },
+                    success: function(response) {
+                        Swal.fire(
+                            'Deleted!',
+                            'The reservation has been deleted.',
+                            'success'
+                        ).then(() => {
+                            location.reload(); // Reload the page after deletion
+                        });
+                    },
+                    error: function() {
+                        Swal.fire(
+                            'Error!',
+                            'There was an error deleting the reservation.',
+                            'error'
+                        );
+                    }
+                });
+            }
         });
     });
+});
 </script>
