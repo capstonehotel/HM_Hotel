202854daeac1d7c262a701e5ac135689257e95d7
capstonehotel <ungonkathleen@gmail.com>
2024-09-11 15:44:02 -0500

    comiit

--- admin/mod_reports/list.php
+++ admin/mod_reports/list.php
@@ -81,6 +81,95 @@
     </div>
 </div>
 
+
+
+<section class="invoice">
+            <div class="row">
+                <div class="col-xs-12">
+                    <h2 class="page-header">
+                        <i class="fa fa-building"></i> HM Hotel Reservation
+                    </h2>
+                </div>
+            </div>
+            <div class="row invoice-info">
+                <div class="col-sm-4 invoice-col">
+                    From
+                    <address>
+                        <strong>HM Hotel Reservation</strong><br>
+                        Crossing Bunakan<br>
+                        Bunakan, Madridejos, Cebu<br>
+                        Phone: 09317622381<br>
+                        Email: Hmhotelreservation@gmail.com
+                    </address>
+                </div>
+                <div class="col-sm-4 invoice-col">
+                    To
+                    <address>
+                        <strong><?php echo $row['G_FNAME'] . ' ' . $row['G_LNAME']; ?></strong><br>
+                        <?php echo $row['G_ADDRESS']; ?><br>
+                        <?php echo $row['G_CITY']; ?><br>
+                        <?php echo $row['G_NATIONALITY']; ?><br>
+                        <?php echo $row['ZIP']; ?>
+                    </address>
+                </div>
+                <div class="col-sm-4 invoice-col">
+                    <b>Invoice No.</b> 00<?php echo $row['GUESTID']; ?><br>
+                    <b>Confirmation ID:</b> <?php echo $row['CONFIRMATIONCODE']; ?><br>
+                    <b>Transaction Date:</b> <?php echo $row['TRANSDATE']; ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-12 table-responsive">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>Room</th>
+                                <th>Description</th>
+                                <th>Price</th>
+                                <th>Checked in</th>
+                                <th>Checked out</th>
+                                <th>Night(s)</th>
+                                <th>Subtotal</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php 
+                            $tot = 0;
+                            while ($row1 = mysqli_fetch_assoc($result1)) {
+                                $days = dateDiff(date($row1['ARRIVAL']), date($row1['DEPARTURE']));
+                                $subtotal = $row1['PRICE'] * ($days == 0 ? 1 : $days);
+                                $tot += $subtotal;
+                                ?>
+                                <tr> 
+                                    <td><?php echo $row1['ACCOMODATION'] . ' ' . $row1['ROOM']; ?></td>
+                                    <td><?php echo $row1['ROOMDESC']; ?><br><?php echo $row1['NUMPERSON']; ?></td>
+                                    <td>&#8369; <?php echo $row1['PRICE']; ?></td>
+                                    <td><?php echo date_format(date_create($row1['ARRIVAL']), 'm/d/Y'); ?></td>
+                                    <td><?php echo date_format(date_create($row1['DEPARTURE']), 'm/d/Y'); ?></td>
+                                    <td><?php echo ($days == 0) ? '1' : $days; ?></td>
+                                    <td>&#8369; <?php echo $subtotal; ?></td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-xs-6">
+                    <p class="lead">Total Amount</p>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <tr>
+                                <th style="width:50%">Total:</th>
+                                <td>&#8369; <?php echo $tot; ?></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        
 <!-- Initialize DataTables -->
 <script>
 $(document).ready(function() {
