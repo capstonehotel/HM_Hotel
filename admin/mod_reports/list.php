1fa179ed201be94ba5dc9f2641a310e51e0e7f2b
capstonehotel <ungonkathleen@gmail.com>
2024-09-11 15:27:10 -0500

    commit

--- admin/mod_reports/list.php
+++ admin/mod_reports/list.php
@@ -16,17 +16,6 @@
     .table-responsive {
         display: none; /* Hide table initially */
     }
-    .print-section {
-        display: none; /* Hide print section initially */
-    }
-    @media print {
-        .print-section {
-            display: block;
-        }
-        .no-print {
-            display: none;
-        }
-    }
 </style>
 
 <div class="container-fluid">
@@ -75,7 +64,7 @@
                                             <td align="center"><?php echo $row['SPRICE']; ?></td>
                                             <td align="center"><?php echo $row['STATUS']; ?></td>
                                             <td align="center">
-                                                <button type="button" class="btn btn-sm btn-primary print-btn" data-code="<?php echo $row['CONFIRMATIONCODE']; ?>"><i class="icon-print"></i> Print</button>
+                                            <a href="printreport.php?code=<?php echo $row['CONFIRMATIONCODE']; ?>" target="_blank" class="btn btn-sm btn-primary"><i class="icon-print"></i> Print</a>
                                                 <?php if($_SESSION['ADMIN_UROLE']=="Administrator"){ ?>
                                                 <button type="button" class="btn btn-danger btn-sm delete-btn" data-id="<?php echo $row['CONFIRMATIONCODE']; ?>"><i class="icon-edit"></i> Delete</button>
                                                 <?php } ?>
@@ -90,123 +79,6 @@
             </div>
         </div>
     </div>
-</div>
-
-<!-- Print Section (hidden by default, shown during print) -->
-<div class="print-section" id="printSection">
-    <?php
-    if (isset($_GET['code']) && !empty($_GET['code'])) {
-        $code = mysqli_real_escape_string($connection, $_GET['code']);
-
-        $query = "SELECT g.`GUESTID`, `G_FNAME`, `G_LNAME`, `G_ADDRESS`, `G_CITY`, `ZIP`, `G_NATIONALITY`, `CONFIRMATIONCODE`, `TRANSDATE`, `ARRIVAL`, `DEPARTURE`, `RPRICE`
-                  FROM `tblguest` g
-                  JOIN `tblreservation` r ON g.`GUESTID` = r.`GUESTID`
-                  WHERE `CONFIRMATIONCODE` = '$code'";
-
-        $result = mysqli_query($connection, $query);
-        if ($result && mysqli_num_rows($result) > 0) {
-            $row = mysqli_fetch_assoc($result);
-        } else {
-            echo 'No records found for the provided confirmation code.';
-            exit;
-        }
-
-        $query1 = "SELECT A.ACCOMID, A.ACCOMODATION, RM.ROOM, RM.ROOMDESC, RM.NUMPERSON, RM.PRICE, RM.ROOMID, RS.ARRIVAL, RS.DEPARTURE 
-                   FROM tblaccomodation A
-                   JOIN tblroom RM ON A.ACCOMID = RM.ACCOMID
-                   JOIN tblreservation RS ON RM.ROOMID = RS.ROOMID 
-                   WHERE RS.CONFIRMATIONCODE = '$code'";
-
-        $result1 = mysqli_query($connection, $query1);
-        ?>
-        <section class="invoice">
-            <div class="row">
-                <div class="col-xs-12">
-                    <h2 class="page-header">
-                        <i class="fa fa-building"></i> HM Hotel Reservation
-                    </h2>
-                </div>
-            </div>
-            <div class="row invoice-info">
-                <div class="col-sm-4 invoice-col">
-                    From
-                    <address>
-                        <strong>HM Hotel Reservation</strong><br>
-                        Crossing Bunakan<br>
-                        Bunakan, Madridejos, Cebu<br>
-                        Phone: 09317622381<br>
-                        Email: Hmhotelreservation@gmail.com
-                    </address>
-                </div>
-                <div class="col-sm-4 invoice-col">
-                    To
-                    <address>
-                        <strong><?php echo $row['G_FNAME'] . ' ' . $row['G_LNAME']; ?></strong><br>
-                        <?php echo $row['G_ADDRESS']; ?><br>
-                        <?php echo $row['G_CITY']; ?><br>
-                        <?php echo $row['G_NATIONALITY']; ?><br>
-                        <?php echo $row['ZIP']; ?>
-                    </address>
-                </div>
-                <div class="col-sm-4 invoice-col">
-                    <b>Invoice No.</b> 00<?php echo $row['GUESTID']; ?><br>
-                    <b>Confirmation ID:</b> <?php echo $row['CONFIRMATIONCODE']; ?><br>
-                    <b>Transaction Date:</b> <?php echo $row['TRANSDATE']; ?>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-12 table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>Room</th>
-                                <th>Description</th>
-                                <th>Price</th>
-                                <th>Checked in</th>
-                                <th>Checked out</th>
-                                <th>Night(s)</th>
-                                <th>Subtotal</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php 
-                            $tot = 0;
-                            while ($row1 = mysqli_fetch_assoc($result1)) {
-                                $days = dateDiff(date($row1['ARRIVAL']), date($row1['DEPARTURE']));
-                                $subtotal = $row1['PRICE'] * ($days == 0 ? 1 : $days);
-                                $tot += $subtotal;
-                                ?>
-                                <tr> 
-                                    <td><?php echo $row1['ACCOMODATION'] . ' ' . $row1['ROOM']; ?></td>
-                                    <td><?php echo $row1['ROOMDESC']; ?><br><?php echo $row1['NUMPERSON']; ?></td>
-                                    <td>&#8369; <?php echo $row1['PRICE']; ?></td>
-                                    <td><?php echo date_format(date_create($row1['ARRIVAL']), 'm/d/Y'); ?></td>
-                                    <td><?php echo date_format(date_create($row1['DEPARTURE']), 'm/d/Y'); ?></td>
-                                    <td><?php echo ($days == 0) ? '1' : $days; ?></td>
-                                    <td>&#8369; <?php echo $subtotal; ?></td>
-                                </tr>
-                            <?php } ?>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-xs-6">
-                    <p class="lead">Amount Paid:</p>
-                    <div class="table-responsive">
-                        <table class="table">
-                            <tr>
-                                <th style="width:50%">Total:</th>
-                                <td>&#8369; <?php echo $tot; ?></td>
-                            </tr>
-                        </table>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <?php
-    }
-    ?>
 </div>
 
 <!-- Initialize DataTables -->
@@ -259,13 +131,6 @@ $(document).ready(function() {
                 });
             }
         });
-    });
-
-    // Event listener for printing the report
-    $(document).on('click', '.print-btn', function() {
-        var code = $(this).data('code');
-        window.location.href = 'list.php?code=' + code; // Reload the page with the code parameter
-        window.print(); // Trigger print dialog
     });
 });
 </script>
