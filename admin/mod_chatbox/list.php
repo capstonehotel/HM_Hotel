746cd1dd3c9365005fa93cd3bbfa65bc29b57de3
capstonehotel <ungonkathleen@gmail.com>
2024-09-17 09:35:59 -0500

    commit

--- admin/mod_chatbox/list.php
+++ admin/mod_chatbox/list.php
@@ -1,3 +1,13 @@
+<!-- Bootstrap CSS -->
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Bootstrap JS and dependencies -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<!-- Font Awesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
 <style>
     .chat-users-list {
     max-height: 400px;
