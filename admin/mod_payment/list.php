0f5af433a100c64a12db9fec34919af1c3b9e62b
capstonehotel <ungonkathleen@gmail.com>
2024-09-17 00:17:11 -0500

    commit

--- admin/mod_payment/list.php
+++ admin/mod_payment/list.php
@@ -1,9 +1,9 @@
 <div class="container-fluid">
     <div class="card shadow mb-4">
         <div class="card-header py-3" style="display: flex; align-items: center;">
-            <h6 class="m-0 font-weight-bold text-primary">Accommodation List</h6>
+            <h6 class="m-0 font-weight-bold text-primary">Payment List</h6>
             <div style="display: flex; width: 100%; justify-content: flex-end;">
-                <a href="index.php?view=add" class="btn btn-sm btn-primary">Add New Accommodation</a>
+                <!-- <a href="index.php?view=add" class="btn btn-sm btn-primary">Add New Accommodation</a> -->
             </div>
         </div>
         <div class="card-body">
@@ -12,7 +12,10 @@
                     <thead>
                         <tr>
                             <th>#</th>
-                            <th>Description</th>
+                            <th>Name</th>
+                            <th>Confirmation Code</th>
+                            <th>Status</th>
+                            <th>Paid</th>
                             <th>Action</th>
                         </tr>
                     </thead>
