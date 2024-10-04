381e05c18913a65d6e0e2cc64f32cd100e9df6c0
capstonehotel <ungonkathleen@gmail.com>
2024-10-04 14:44:12 -0500

    d

--- booking/otp_verify.php
+++ booking/otp_verify.php
@@ -1,13 +1,12 @@
 <?php
-// verify-otp.php
-
-
-$enteredOtp = $_POST['otp'];
-$storedOtp = $_SESSION['otp'];
-
-if ($enteredOtp === $storedOtp) {
-  echo 'true';
-} else {
-  echo 'false';
+if (isset($_POST['otp'])) {
+    $otp = $_POST['otp'];
+    if ($otp == $_SESSION['otp']) {
+        // OTP is valid, return success message
+        echo 'valid';
+    } else {
+        // OTP is invalid, return error message
+        echo 'invalid';
+    }
 }
 ?>
