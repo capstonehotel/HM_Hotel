f150a38bed409f87d2d1fd28bb3d855a7023df6c
capstonehotel <ungonkathleen@gmail.com>
2024-10-04 15:51:29 -0500

    s

--- booking/personalinfo.php
+++ booking/personalinfo.php
@@ -66,7 +66,7 @@ if (isset($_POST['submit'])){
   
 // redirect('index.php?view=payment');
 $_SESSION['otp'] = sendOTP($_SESSION['username'],$_SESSION['name'], $_SESSION['last']);
-var_dump($_SESSION['otp']);
+
         // echo '<script>$("#otp-modal").modal("show");</script>';
         // Redirect to payment page
          redirect('index.php?view=payment&verify');
