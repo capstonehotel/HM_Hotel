6d80d75dbf579136962862bfe760c3cfe781acb7
capstonehotel <ungonkathleen@gmail.com>
2024-10-05 00:44:51 -0500

    gg

--- booking/otp_verify.php
+++ booking/otp_verify.php
@@ -11,7 +11,7 @@ if (isset($_POST['otp'])) {
         // Verify that the OTP code is valid
         if (preg_match('/^[0-9]{6}$/', $userOtp)) {
             // User-input OTP is a valid OTP code
-            if (isset($_SESSION['otp']) && $_SESSION['otp'] !== '' && $_SESSION['otp'] === $userOtp) {
+            if ($_SESSION['otp'] === $userOtp) {
                 var_dump($_SESSION['otp']); // Verify that the OTP is being retrieved correctly from the session
                 if ($userOtp == $_SESSION['otp']) {
                     // OTP is valid, return success message
@@ -21,7 +21,7 @@ if (isset($_POST['otp'])) {
                     echo 'invalid';
                 }
             } else {
-                echo $userOtp.'OTP not found in session or OTP is empty or OTP is invalid'.$_SESSION['otp']; // Handle the case where the OTP is not found in the session or OTP is empty or OTP is invalid
+                echo 'OTP not found in session or OTP is empty or OTP is invalid'; // Handle the case where the OTP is not found in the session or OTP is empty or OTP is invalid
             }
         } else {
             echo 'Invalid OTP code'; // Handle the case where the user-input OTP is not a valid OTP code
