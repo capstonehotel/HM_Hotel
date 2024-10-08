3070d202b10763ede0540f8c4486246c48c64a72
capstonehotel <ungonkathleen@gmail.com>
2024-10-08 14:55:06 -0500

    c

--- admin/login.php
+++ admin/login.php
@@ -193,7 +193,7 @@ if (isset($_POST['btnlogin'])) {
             </div>
         </div>
     </div> 
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
                  <script>
     document.addEventListener('DOMContentLoaded', function() {
         function detectXSS(inputField, fieldName) {
