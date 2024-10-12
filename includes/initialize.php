58482f97db89e9416daa71c4ba4e6e40200457b1
capstonehotel <ungonkathleen@gmail.com>
2024-10-12 01:09:16 -0500

    Update initialize.php

--- includes/initialize.php
+++ includes/initialize.php
@@ -17,7 +17,7 @@ defined('SITE_ROOT') ? null : define ('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'].DS.
 defined('LIB_PATH') ? null : define ('LIB_PATH',SITE_ROOT.DS.'includes');
 
 // load config file first 
-require ("config.php");
+require_once("config.php");
 //load basic functions next so that everything after can use them
 require_once("functions.php");
 //later here where we are going to put our class session
